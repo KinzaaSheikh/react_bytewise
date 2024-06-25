a5933bdaee07c8dc7a4fe597d427b0bfbeea18d4
Kinza Sheikh <75093626+KinzaaSheikh@users.noreply.github.com>
2024-06-26 00:45:26 +0500

    Debugged the jobs page data loading problem

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,49 +1,46 @@
-import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
 
-  if (!job) {
-    return <div>No job data available</div>;
-  }
+  let description = job.description;
 
-  let description = job.description || '';
-
-  if (!showFullDescription && description.length > 90) {
-    description = description.substring(0, 90) + "...";
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + '...';
   }
 
   return (
-    <div className="bg-white rounded-xl shadow-md relative">
-      <div className="p-4">
-        <div className="mb-6">
-          <div className="text-gray-600 my-2">{job.type}</div>
-          <h3 className="text-xl font-bold">{job.title}</h3>
+    <div className='bg-white rounded-xl shadow-md relative'>
+      <div className='p-4'>
+        <div className='mb-6'>
+          <div className='text-gray-600 my-2'>{job.type}</div>
+          <h3 className='text-xl font-bold'>{job.title}</h3>
         </div>
-        <div className="mb-5">
-          {description}
-        </div>
+
+        <div className='mb-5'>{description}</div>
+
         <button
           onClick={() => setShowFullDescription((prevState) => !prevState)}
-          className="text-indigo-500 mb-5 hover:text-indigo-600"
+          className='text-indigo-500 mb-5 hover:text-indigo-600'
         >
           {showFullDescription ? 'Less' : 'More'}
         </button>
-        <h3 className="text-indigo-500 mb-2">
-          {job.salary}/Year
-        </h3>
-        <div className="border border-gray-100 mb-5"></div>
-        <div className="flex flex-col lg:flex-row justify-between mb-4">
-          <div className="text-orange-700 mb-3">
-            <FaMapMarker className='inline text-lg mb-1 mr-2' />
+
+        <h3 className='text-indigo-500 mb-2'>{job.salary} / Year</h3>
+
+        <div className='border border-gray-100 mb-5'></div>
+
+        <div className='flex flex-col lg:flex-row justify-between mb-4'>
+          <div className='text-orange-700 mb-3'>
+            <FaMapMarker className='inline text-lg mb-1 mr-1' />
             {job.location}
           </div>
           <Link
-            to={`/job/${job.id}`}
-            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            to={`/jobs/${job.id}`}
+            className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'
           >
             Read More
           </Link>
@@ -52,16 +49,4 @@ const JobListing = ({ job }) => {
     </div>
   );
 };
-
-JobListing.propTypes = {
-  job: PropTypes.shape({
-    type: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    salary: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  }).isRequired,
-};
-
 export default JobListing;
--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,9 +1,9 @@
-import JobListing from "../components/JobListing"
+import JobListings from "../components/JobListings"
 
 const JobsPage = () => {
   return (
     <section className="bg-blue-50 px-4 py-6">
-      <JobListing />
+      <JobListings />
     </section>
   );
 }
